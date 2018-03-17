0fe91549873e6a10895a264287897fb88a7eaff7
mouda.xue <mouda.xue@alibaba-inc.com>
2018-03-17 22:16:28 +0800

    init some mybatis demo configuration
    init some mybatis demo configuration

---
+++ src/main/java/com/sit/mybatis/mapper/RoleMapper.java
@@ -0,0 +1,7 @@
+package com.sit.mybatis.mapper;
+
+import com.sit.mybatis.po.Role;
+
+public interface RoleMapper {
+    public Role getRole(Long id);
+}
---
+++ src/main/java/com/sit/mybatis/util/SqlSessionFactoryUtil.java
@@ -0,0 +1,4 @@
+package com.sit.mybatis.util;
+
+public class SqlSessionFactoryUtil {
+}
